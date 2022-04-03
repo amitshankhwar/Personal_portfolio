ac5b1dd69a433f366d5ae278b229ea6a4aad8b96
Abhirams2020 <abhirams2020@gmail.com>
2022-04-03 11:07:22 +0530

    fixed resume not showing

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -9,7 +9,7 @@ import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 const resumeLink =
-  "https://raw.githubusercontent.com/abhirams2020/abhirams2020_portfolio/main/Abhiram_S_Resume_frntd.pdf";
+  "https://raw.githubusercontent.com/abhirams2020/abhirams2020_portfolio/main/src/Assets/resume.pdf";
 
 function ResumeNew() {
   const [width, setWidth] = useState(1200);
